fab9adac630f82e8735b3f543931597be8bd0258
Sarthak Gaud <115076922+iminoaru@users.noreply.github.com>
2023-08-06 23:15:52 +0530

    Create 11-sleep.js

---
+++ 11-sleep.js
@@ -0,0 +1,10 @@
+
+async function sleep(millis) {
+    return new Promise(delayresolve => setTimeout(delayresolve, millis));
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
